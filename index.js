df32d22758a8dcdcc218bb044efd799cf3c7e6a5
meetaza <anggit@cleva.id>
2022-01-27 17:34:10 +0700

    JWT, Add user, Login, Auth

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require ("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routers/user");
+const authRoute = require("./routers/auth");
 
 dotenv.config();
 
@@ -14,7 +15,8 @@ mongoose
     });
 
     app.use(express.json());
-    app.use("/api/user", userRoute);
+    app.use("/api/auth", authRoute);
+    app.use("/api/users", userRoute);
 
 app.listen(process.env.PORT || 5000, () =>{
     console.log("Backend Server Is Running")
