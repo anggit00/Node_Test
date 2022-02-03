4b760416966fb8da4cf00ef7fa14d6c3e3224787
Anggit Akbar Maulana <anggitakbar00@gmail.com>
2022-02-03 08:23:27 +0700

    add order

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const dotenv = require("dotenv");
 const userRoute = require("./routers/user");
 const authRoute = require("./routers/auth");
 const productRoute = require("./routers/product");
+const cartRoute = require("./routers/cart");
+const orderRoute = require("./routers/order");
 
 dotenv.config();
 
@@ -19,6 +21,8 @@ mongoose
     app.use("/api/auth", authRoute);
     app.use("/api/users", userRoute);
     app.use("/api/products", productRoute);
+    app.use("/api/carts", cartRoute);
+    app.use("/api/orders", orderRoute);
 
 app.listen(process.env.PORT || 5000, () =>{
     console.log("Backend Server Is Running")
